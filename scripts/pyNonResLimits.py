29c1fbc37d6f83c53c0b676d2a1d77999fbbce8a
Andrey Pozdnyakov <Andrey.Pozdnyakov@cern.ch>
2016-10-21 15:23:47 +0200

    Runninng in parralel

--- scripts/pyNonResLimits.py
+++ scripts/pyNonResLimits.py
@@ -17,6 +17,8 @@ parser.add_argument('--overwrite', dest="overwrite", action="store_true", defaul
                     help="Overwrite the results into the same directory")
 parser.add_argument("-v", dest="verb", type=int, default=0,
                     help="Verbosity level: 0 - Minimal; 1 - Talk to me; 2 - talk more, I like to listen; 3 - not used; 4 - Debug messages only (minimal of other messages)")
+parser.add_argument('-j', '--ncpu',dest="ncpu", type=int, default=2,
+                    help="Number of cores to run on.")
 
 opt = parser.parse_args()
 #opt.func()
@@ -30,20 +32,19 @@ if opt.verb in [0,4]:
   RooMsgService.instance().setGlobalKillBelow(RooFit.FATAL)
   RooMsgService.instance().setSilentMode(True)
 
-workingPath = os.getcwd()
-parentDir = os.path.abspath(os.path.join(workingPath, os.pardir))
-
-begin = time.clock()
-
+begin = time.time()
+  
 def createDir(myDir):
   if opt.verb>0 and opt.verb<4: print 'Creating a new directory: ', myDir
   if os.path.exists(myDir):
-    if opt.verb>0 and opt.verb<4:
-      print "\t OOps, this directory already exists:", myDir
+    if opt.verb<4:
+      print "\t This directory already exists:", myDir
     if opt.overwrite:
-      if opt.verb>0 and opt.verb<4:
+      if opt.verb<4:
         print ' But we will continue anyway (--rewrite it!)'
     else:
+      if opt.verb<4:
+        print ' And so I exit this place...'
       sys.exit(1)
   else:
     try: os.makedirs(myDir)
@@ -51,7 +52,12 @@ def createDir(myDir):
       if os.path.isdir(myDir): pass
       else: raise
 
-def runCombine(inDir, doBlind, asimov=False):
+def printTime(t1, t2):
+  tNew = time.clock()
+  print 'Time since start of worker: %.2f sec; since previous point: %.2f sec' %(tNew-t2,tNew-t1)
+  return tNew
+
+def runCombine(inDir, doBlind, Label = None, asimov=False):
   print 'Running combine tool.  Dir:', inDir, 'Blind:', doBlind
   if opt.verb==4:
     print 'inDir should be the immediate directory where the card is located'
@@ -65,16 +71,35 @@ def runCombine(inDir, doBlind, asimov=False):
   if asimov:
     asimovOpt = '--X-rtd TMCSO_AdaptivePseudoAsimov=50'
   
-  cardName  = inDir+"/hhbbgg_13TeV_DataCard.txt"
-  logFile   = inDir+"/log.txt"
-  outDir    = inDir
+  cardName = inDir+"/hhbbgg_13TeV_DataCard.txt"
+  logFile  = inDir+"/result.log"
 
-  command1="combine -M Asymptotic -m 125 "+blinded+" "+asimovOpt+" --out "+outDir+" "+cardName+" >>"+logFile
+  command1 = "combine -M Asymptotic -m 125 -n "+Label+" "+blinded+" "+asimovOpt+" "+cardName+" > "+logFile+" 2>&1"
   os.system(command1)
+
+  fName = 'higgsCombine'+Label+'.Asymptotic.mH125.root'
+  
+  outDir = inDir
+  os.rename(fName, outDir+'/'+fName)
 
 
 def runFullChain(Params, NRnode=None, NRgridPoint=None):
-  #print sys._getframe().f_code.co_name
+  #print 'Running: ', sys._getframe().f_code.co_name, " Node=",NRnode
+  # print sys._getframe().f_code
+
+  if opt.verb>1:
+    print 'Node=',NRnode, 'gridPoint=',NRgridPoint,'Options:\n ', opt, 
+  
+  if NRnode!=None and NRgridPoint!=None:
+    print 'WARning: cannot have both the Node and grid Point. Chose one and try again'
+    sys.exit(1)
+  elif NRnode!=None:
+    Label = "_Node_"+NRnode
+  elif NRgridPoint!=None:
+    Label = "_gridPoint_"+NRgridPoint
+  else:
+    print 'WARning: must provide one of these: NRnode or NRgridPoint'
+    sys.exit(1)
 
   start = time.clock()
 
@@ -88,7 +113,7 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
   addHiggs   = Params['other']["addHiggs"]
   doBlinding = Params['other']["doBlinding"]
   doBands = Params['other']["doBands"]
-  NCAT = Params['other']["ncat"]
+  NCAT    = Params['other']["ncat"]
   doBrazilianFlag = Params['other']["doBrazilianFlag"]
 
   if NCAT > 3:
@@ -104,10 +129,8 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
   obs  = Params['other']["obs"]
   twotag=Params['other']["twotag"]
 
-
   dataDir  = os.getenv("CMSSW_BASE")+Params['data']['dir']
   dataName = Params['data']['name']
-
 
   sigCat = 1
   sigMas = '125'
@@ -131,12 +154,13 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
 
 
   for t in signalTypes:
-    newFolder = baseFolder+ str('/'+t+"_Node_"+NRnode)
-    print 'Type = ', t, newFolder
+    newFolder = baseFolder+ str('/'+t+Label)
+    if opt.verb>1:
+      print 'Type = ', t, newFolder
 
     createDir(newFolder)
 
-    HLFactoryname= str(t+"_Node_"+NRnode)
+    HLFactoryname= str(t+Label)
     hlf = RooStats.HLFactory(HLFactoryname, signalModelCard, False)
     w = hlf.GetWs()
 
@@ -158,35 +182,24 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
     if openStatus==-1:
       print 'There is a problem with openStatus'
       sys.exit(1)
-    print "\t SIGNAL ADDED"
-    print 'Signal of node '+n+' and type '+ t +' added!'
-    if opt.verb>0:
-      p1 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p1-start,p1-start)
-
-
+    print "\t SIGNAL ADDED. Node=",NRnode, 'type=',t
+    if opt.verb>0: p1 = printTime(start, start)
+    
     createDir(newFolder+'/workspaces')
     createDir(newFolder+'/datacards')
 
     theFitter.SigModelFit( mass)
-    print "\t SIGNAL FITTED"
-    if opt.verb>0:
-      p1ex = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p1ex-start,p1ex-p1)
+    print "\t SIGNAL FITTED. Node=",NRnode, 'type=',t
+    if opt.verb>0: p2 = printTime(p1,start)
 
     fileBaseName = "hhbbgg.mH"+str(mass)[0:3]+"_13TeV"
     theFitter.MakeSigWS( fileBaseName)
-    print "\t SIGNAL'S WORKSPACE DONE"
-    if opt.verb>0:
-      p2 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p2-start,p2-p1ex)
+    print "\t SIGNAL'S WORKSPACE DONE. Node=",NRnode, 'type=',t
+    if opt.verb>0: p3 = printTime(p2,start)
 
     theFitter.MakePlots( mass)
-    print "\t SIGNAL'S PLOT DONE"
-    if opt.verb>0:
-      p3 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p3-start,p3-p2)
-
+    print "\t SIGNAL'S PLOT DONE.. Node=",NRnode, 'type=',t
+    if opt.verb>0: p4 = printTime(p3,start)
 
     if addHiggs:
       print 'Here will add SM Higgs contributions'
@@ -195,19 +208,15 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
     ddata = str(dataDir + '/LT_'+dataName+'.root')
 
     theFitter.AddBkgData(ddata)
-    print "\t BKG ADDED"
-    if opt.verb>0:
-      p4 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p4-start,p4-p3)
+    print "\t BKG ADDED. Node=",NRnode, 'type=',t
+    if opt.verb>0: p4 = printTime(p3,start)
 
     if opt.verb==3:
       TheFitter.PrintWorkspace();
 
     fitresults = theFitter.BkgModelFit( doBands, addHiggs)
-    print "\t BKG FITTED"
-    if opt.verb>0:
-      p5 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p5-start,p5-p4)
+    print "\t BKG FITTED. Node=",NRnode, 'type=',t
+    if opt.verb>0: p5 = printTime(p4,start)
     if fitresults==None:
       print "PROBLEM with fitresults !!"
       sys.exit(1)
@@ -217,11 +226,8 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
 
     wsFileBkgName = "hhbbgg.inputbkg_13TeV"
     theFitter.MakeBkgWS( wsFileBkgName);
-    print "\t BKG'S WORKSPACE DONE"
-    if opt.verb>0:
-      p6 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p6-start,p6-p5)
-
+    print "\t BKG'S WORKSPACE DONE. Node=",NRnode, 'type=',t
+    if opt.verb>0: p6 = printTime(p5,start)
 
     # This is making cards ala 8 TeV. We don't need this for now
     #theFitter.MakeDataCard( fileBaseName, wsFileBkgName, useSigTheoryUnc)
@@ -251,19 +257,19 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
     if opt.verb==4:
       print DCcommand
     os.system(DCcommand)
-    print "\t DATACARD DONE"
+    print "\t DATACARD DONE. Node=",NRnode, 'type=',t
 
-    if opt.verb>0:
-      p7 = time.clock()
-      print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p7-start,p7-p6)
+    if opt.verb>0: p7 = printTime(p6,start)
 
-      
+    # End of loop over Types
+  ## <-- indent
+  
   # Here we shall merge datacars of all categories (in this case two)
   cardsToMerge = ''
   for t in signalTypes:
-    cardsToMerge += baseFolder+'/'+t+"_Node_"+NRnode+'/datacards/hhbbgg_13TeV_DataCard.txt '
+    cardsToMerge += baseFolder+'/'+t+Label+'/datacards/hhbbgg_13TeV_DataCard.txt '
 
-  newDir = baseFolder+'/CombinedCard_Node_'+NRnode
+  newDir = baseFolder+'/CombinedCard'+Label
   createDir(newDir)
 
   combCard = newDir+'/hhbbgg_13TeV_DataCard.txt'
@@ -271,24 +277,23 @@ def runFullChain(Params, NRnode=None, NRgridPoint=None):
 
   # Now we actually need to fix the combined card
   for t in signalTypes:
-    strReplace = baseFolder+'/'+t+"_Node_"+NRnode+'/datacards/'
+    strReplace = baseFolder+'/'+t+Label+'/datacards/'
     os.system("sed -i 's|"+strReplace+"./|./|g' "+combCard)
   
   if doCombine:
-    runCombine(newDir, doBlinding)
-    print "\t COMBINE DONE"
-  if opt.verb>0:
-    p8 = time.clock()
-    print 'Time since beginning: %.2f sec; since previous point: %.2f sec' %(p8-start,p8-p7)
+    runCombine(newDir, doBlinding, Label=Label)
+    print "\t COMBINE DONE. Node=",NRnode
+  if opt.verb>0: p8 = printTime(p7,start)
 
-
-
+  
 if __name__ == "__main__":
   print "This is the __main__ part"
 
   gSystem.Load('libHiggsAnalysisbbggLimits')
 
-  if opt.verb: print workingPath
+  #workingPath = os.getcwd()
+  # parentDir = os.path.abspath(os.path.join(workingPath, os.pardir))
+  #if opt.verb: print workingPath
 
   with open(opt.fname, 'r') as fp:
     Params = json.load(fp)
@@ -300,11 +305,35 @@ if __name__ == "__main__":
 
   nodes = Params['signal']['nodes']
 
-  for n in nodes:
-    print 'Node = ', n
-    runFullChain(Params, n)
+  if opt.outDir:
+    baseFolder=outDir+"_v"+str(Params['other']["version"])
+  else:
+    baseFolder="./bbggToolsResults_v"+str(Params['other']["version"])
 
+  createDir(baseFolder)
+    
+  from multiprocessing import Pool, TimeoutError  
+  pool = Pool(processes=opt.ncpu)
+
+  if opt.NRW:
+    print 'Running over 5D space points'
+    for p in xrange(0,10):
+      pool.apply_async(runFullChain, args = (Params, None,p,))
+  else:
+    print 'Running over nodes'
+    for n in nodes:
+      #for n in ['2','SM']:
+      # print 'Node = ', n
+    
+      # Run on multiple cores:
+      pool.apply_async(runFullChain, args = (Params, n,))
+
+      # Use signle core:
+      #runFullChain(Params, NRnode=n)
+    
+  pool.close()  
+  pool.join()
 
   if opt.verb>0:
-    end = time.clock()
+    end = time.time()
     print '\t Total Time: %.2f'%(end-begin)
