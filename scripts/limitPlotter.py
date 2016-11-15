691ec66aed6587b1f1414e664b4d6a461988ada9
Andrey Pozdnyakov <Andrey.Pozdnyakov@cern.ch>
2016-11-15 15:55:49 +0100

    Limit plotter doing good plots

--- scripts/limitPlotter.py
+++ scripts/limitPlotter.py
@@ -50,6 +50,12 @@ def getValuesFromFile(fname):
 if __name__ == "__main__":
   print "This is the __main__ part"
   
+  #gROOT.ProcessLine(".L ./tdrstyle.C")
+  gROOT.LoadMacro("./CMS_lumi.C")
+  #setTDRStyle()
+  #gROOT.ForceStyle()
+  TH1.SetDefaultSumw2(kTRUE)
+
   xAxis = []
   xErr  = []
   obs   = []
@@ -59,6 +65,7 @@ if __name__ == "__main__":
   exp2SigHi  = []
   exp2SigLow = []
   
+  missedPoints = []
 
   if opt.x == 'res':
     print 'This option is not implemented yet:', opt.x
@@ -70,6 +77,10 @@ if __name__ == "__main__":
 
     for i,n in enumerate(myNodes):
       l = getValuesFromFile(opt.inDir+'/CombinedCard_Node_'+n+'/higgsCombine_Node_'+n+'.Asymptotic.mH125.root')
+      if l==None:
+        missedPoints.append(n)
+        continue
+
       exp2SigLow.append(l[0])
       exp1SigLow.append(l[1])
       expMean.append(l[2])
@@ -88,7 +99,8 @@ if __name__ == "__main__":
     
     for n in xrange(0,1507):
       l = getValuesFromFile(opt.inDir+'/CombinedCard_gridPoint_'+str(n)+'/higgsCombine_gridPoint_'+str(n)+'.Asymptotic.mH125.root')
-      if l==None:
+      if l==None or len(l)!=6:
+        missedPoints.append(n)
         continue
 
       exp2SigLow.append(l[0])
@@ -104,14 +116,20 @@ if __name__ == "__main__":
       xAxis.append(float(n))
       xErr.append(0.5)
 
+
   if len(xAxis) == 0:
-    print 'There are no point to plot! Exiting...'
+    print 'There are no points to plot! Exiting...'
     sys.exit(0)
 
+
+  print 'Missed points:', misssedPoints
   # Create the arrays for graphs
   zeros_Array = np.zeros(len(xAxis),dtype = float)
   xAxis_Array = np.array(xAxis)
-  xErr_Array = zeros_Array
+  xErr_Array  = zeros_Array
+
+  if opt.x=='grid':
+    xErr_Array = np.array(xErr)
 
   obs_Array = np.array(obs)
   exp_Array = np.array(expMean)
@@ -124,48 +142,91 @@ if __name__ == "__main__":
   mg.SetTitle('')
 
   nPoints  = len(xAxis)
-  expected = TGraphAsymmErrors(nPoints,xAxis_Array,exp_Array,xErr_Array,xErr_Array,zeros_Array,zeros_Array)
+  expected = TGraphAsymmErrors(nPoints,xAxis_Array,exp_Array,zeros_Array,zeros_Array,zeros_Array,zeros_Array)
   oneSigma = TGraphAsymmErrors(nPoints,xAxis_Array,exp_Array,xErr_Array,xErr_Array,exp1SigLowErr_Array,exp1SigHiErr_Array)
   twoSigma = TGraphAsymmErrors(nPoints,xAxis_Array,exp_Array,xErr_Array,xErr_Array,exp2SigLowErr_Array,exp2SigHiErr_Array)
   observed = TGraphAsymmErrors(nPoints,xAxis_Array,obs_Array,zeros_Array,zeros_Array,zeros_Array,zeros_Array)
-  oneSigma.SetFillColor(kGreen)
-  twoSigma.SetFillColor(kYellow)
-  
-  expected.SetMarkerColor(kBlack)
-  #expected.SetMarkerStyle(kFullCircle)
-  #expected.SetMarkerSize(1.5)
-  expected.SetLineColor(kBlack)
-  expected.SetLineWidth(2)
-  expected.SetLineStyle(2)
 
-  observed.SetLineWidth(2)
 
-  mg.Add(twoSigma)
-  mg.Add(oneSigma)
-  mg.Add(expected)
-  mg.Add(observed,'P')
+
+  if opt.x=='nodes':
+
+    twoSigma.SetLineWidth(8)
+    twoSigma.SetLineColor(kYellow)
+
+    oneSigma.SetMarkerColor(kBlue+1)
+    oneSigma.SetMarkerStyle(21)
+    oneSigma.SetLineColor(kGreen+1)
+    oneSigma.SetLineWidth(7)
+
+    observed.SetMarkerStyle(20)
+
+    mg.Add(twoSigma,'PZ')
+    mg.Add(oneSigma, 'EPZ')
+    
+    mg.Add(observed)
+
+    mg.Draw('APZ')
+
+
+  if opt.x=='grid':
+    
+    twoSigma.SetLineColor(kYellow)
+    twoSigma.SetLineWidth(1)
+    twoSigma.SetFillColor(kYellow)
+
+    oneSigma.SetLineColor(kGreen+1)
+    oneSigma.SetFillColor(kGreen+1)
+
+    expected.SetLineColor(kBlue+1)
+    expected.SetLineWidth(2)
+
+    observed.SetMarkerStyle(21)
+    observed.SetMarkerSize(1)
+
+
+    mg.Add(twoSigma)
+    mg.Add(oneSigma)
+    mg.Add(expected,'L')
+
+    mg.Add(observed,'L')
+
+    mg.Draw('A')
+
 
   mg.SetMinimum(0)
-  
-  mg.Draw('AL3')
-  mg.GetXaxis().SetTitle('m_{H} (GeV)')
-  mg.GetXaxis().SetLimits(xAxis_Array[0],xAxis_Array[-1])
+
+  mg.GetXaxis().SetTitle('Node Number')
+  if opt.x=='nodes':
+    mg.GetXaxis().SetLimits(-1, 14)
+  if opt.x=='grid':
+    mg.GetXaxis().SetLimits(-10, 1520)
   mg.GetYaxis().SetTitle('#sigma(pp #rightarrow HH) #times B(HH #rightarrow bb#gamma#gamma)_{95% CL} (fb)')
   mg.SetMaximum(50)
 
   gPad.RedrawAxis()
 
 
-  leg = TLegend(0.50,0.66,0.75,0.89)
-  leg.AddEntry(observed,"Observed", "l")
-  leg.AddEntry(expected,"Expected", "l")
-  leg.AddEntry(oneSigma,"Expected #pm 1#sigma", "f")
-  leg.AddEntry(twoSigma,"Expected #pm 2#sigma", "f")
-
+  leg = TLegend(0.60,0.66,0.85,0.89)
   leg.SetTextFont(42)
   leg.SetTextSize(0.04)
-  leg.SetFillColor(kWhite)
+  leg.SetFillStyle(0)
+  leg.SetBorderSize(0)
+
+  if opt.x=='nodes':
+    leg.AddEntry(observed,"Observed", "p")
+    leg.AddEntry(oneSigma,"Expected", "p")
+    leg.AddEntry(oneSigma,"Expected #pm 1#sigma", "l")
+    leg.AddEntry(twoSigma,"Expected #pm 2#sigma", "l")
+  if opt.x=='grid':
+    leg.AddEntry(observed,"Observed", "p")
+    leg.AddEntry(expected,"Expected", "l")
+    leg.AddEntry(oneSigma,"Expected #pm 1#sigma", "f")
+    leg.AddEntry(twoSigma,"Expected #pm 2#sigma", "f")
+
+
   leg.Draw()
 
-  for e in ['.png', '.pdf']:
+  CMS_lumi(c1, 4, 11, "")
+  for e in ['.png']:
     c1.SaveAs(opt.inDir+'/limitPlot_'+opt.x+e)  
