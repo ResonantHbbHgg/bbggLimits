64d1494793807f219b9824948d648610cb6736d8
maxime <mgouzevi@cern.ch>
2017-11-13 19:24:29 +0100

    Updated

--- scripts/MakeKLambdaScan.py
+++ scripts/MakeKLambdaScan.py
@@ -1,5 +1,6 @@
 from ROOT import *
 import argparse, os
+from math import sqrt
 from HiggsAnalysis.bbggLimits.NiceColors import *
 import HiggsAnalysis.bbggLimits.CMS_lumi as CMS_lumi
 from HiggsAnalysis.bbggLimits.MyCMSStyle import *
@@ -8,6 +9,18 @@ from HiggsAnalysis.bbggLimits.DefineScans import *
 gROOT.SetBatch()
 
 A13tev = [2.09078, 10.1517, 0.282307, 0.101205, 1.33191, -8.51168, -1.37309, 2.82636, 1.45767, -4.91761, -0.675197, 1.86189, 0.321422, -0.836276, -0.568156]
+
+errUp =  7.293147e+00/100 # theoretical uncertainty on HH cross section from YR4
+errDo =  8.408329e+00/100 # theoretical uncertainty on HH cross section from YR4
+
+
+
+#coefxmin = (A13tev[0]+16*A13tev[2]+4*A13tev[6])
+#Observed/Expected 3.07490022078 Th 3.0520579135 klambda -7.34
+#Observed/Expected 4.29277026057 Th 4.33557538187 klambda 8.02
+#coefxmin  kt = 0.411199557203*Lambda corresponds to the minimum of the cross section
+print abs(-3.1)
+
 
 def functionGF(kl,kt,c2,cg,c2g,A): return A[0]*kt**4 + A[1]*c2**2 + (A[2]*kt**2 + A[3]*cg**2)*kl**2  + A[4]*c2g**2 + ( A[5]*c2 + A[6]*kt*kl )*kt**2  + (A[7]*kt*kl + A[8]*cg*kl )*c2 + A[9]*c2*c2g  + (A[10]*cg*kl + A[11]*c2g)*kt**2+ (A[12]*kl*cg + A[13]*c2g )*kt*kl + A[14]*cg*c2g*kl
 
@@ -74,24 +87,67 @@ for kl in scan_kl['kl']:
     continue
   tfile.Close()
 
+
+def kmax_finder(exp):
+
+# for this algo we use the scaling property: 
+#sigma = kt^4*(A0 + A2*(kl/kt)^2 + A6*(kl/kt))
+
+  kmax = -100
+  klist = []
+  kl, sigma = Double(0), Double(0)
+  for ikl,dummy in enumerate(myKl):
+    exp.GetPoint(ikl,kl,sigma)
+    s0 = 33.45*0.0026*functionGF(kl, 1, 0.0, 0.0, 0.0, A13tev)    
+    kt = pow(sigma/s0, 1/4.)
+    print 'kl ',kl, 'sigma ',sigma, ' kmax ', kmax 
+    if kt > kmax:
+      kmax = kt
+      
+  return kmax
+
+
+
+
 nonresXSEC = TGraphAsymmErrors()
 nonresXSEC.SetName("nonresXsec")
 nonresXSEC_2 = TGraphAsymmErrors()
 nonresXSEC_2.SetName("nonresXsec_2")
+nonresXSEC_3 = TGraphAsymmErrors()
+nonresXSEC_3.SetName("nonresXsec_3")
 for ikl,kl in enumerate(myKl):
   xsec = 33.45*0.0026*functionGF(kl, 1.0, 0.0, 0.0, 0.0, A13tev)
-  xsec_2 = 33.45*0.0026*functionGF(kl*2.0, 2.0, 0.0, 0.0, 0.0, A13tev)
+  xsec_2 = 33.45*0.0026*functionGF(kl*2, 2, 0.0, 0.0, 0.0, A13tev)
+#  xsec_3 = 33.45*0.0026*functionGF(kl, coefxmin*kl, 0.0, 0.0, 0.0, A13tev)
+#  valkl = kl*kl*coefxmin
+#  valkt = kl*coefxmin
+  kmax = -100
+
   nonresXSEC.SetPoint(ikl, kl, xsec)
-  nonresXSEC.SetPointError(ikl, 0,0, xsec*0.1, xsec*0.1)
+  nonresXSEC.SetPointError(ikl, 0,0, xsec*errDo, xsec*errUp)
   nonresXSEC_2.SetPoint(ikl, kl, xsec_2)
-  nonresXSEC_2.SetPointError(ikl, 0,0, xsec_2*0.1, xsec_2*0.1)
+  nonresXSEC_2.SetPointError(ikl, 0,0, xsec_2*errUp, xsec_2*errDo)
+
+
+
   for qt in quantiles:
     if '-1' in qt:
       plots[qt].SetPoint(ikl, kl, lims['-1'][ikl])
+#      print ikl, ' kl ', kl , ' lim ',  lims['-1'][ikl]
     else:
       plots[qt].SetPoint(ikl, kl, lims['0.500'][ikl])
   plots['0.160'].SetPointError(ikl, 0,0, lims['0.500'][ikl] - lims['0.160'][ikl], lims['0.840'][ikl] - lims['0.500'][ikl] ) 
   plots['0.025'].SetPointError(ikl, 0,0, lims['0.500'][ikl] - lims['0.025'][ikl], lims['0.975'][ikl] - lims['0.500'][ikl] ) 
+
+
+kmax = kmax_finder(plots['-1'])
+print 'kmax ', kmax
+
+for ikl,kl in enumerate(myKl):
+  xsec_3 = 33.45*0.0026*functionGF(kl*kmax, kmax, 0.0, 0.0, 0.0, A13tev)
+  nonresXSEC_3.SetPoint(ikl, kl, xsec_3)
+  nonresXSEC_3.SetPointError(ikl, 0,0, xsec_3*errDo, xsec_3*errUp)
+
 
 
 SetGeneralStyle()
@@ -121,6 +177,8 @@ nonresXSEC.SetLineColor(th_col)
 nonresXSEC.SetFillColorAlpha(th_col, 0.5)
 nonresXSEC_2.SetLineColor(cNicePurple)
 nonresXSEC_2.SetFillColorAlpha(cNicePurple, 0.5)
+nonresXSEC_3.SetLineColor(cNiceGreen)
+nonresXSEC_3.SetFillColorAlpha(cNiceGreen, 0.5)
 plots['0.025'].Draw("A3Z")
 
 plots['0.025'].GetXaxis().SetTitle('#kappa_{#lambda}/#kappa_{t}')
@@ -136,11 +194,13 @@ plots['0.500'].Draw("LZ same")
 if(opt.unblind): plots['-1'].Draw("CZ same")
 nonresXSEC.Draw("3ZL same")
 nonresXSEC_2.Draw("3ZL same")
+nonresXSEC_3.Draw("3ZL same")
 
 ltx = TLatex()
 ltx.SetNDC()
+ltx.SetTextAlign(32)
 ltx.SetTextSize(.035)
-ltx.DrawLatex(0.45,0.83,'#font[61]{pp#rightarrowHH#rightarrow#gamma#gammab#bar{b}}')
+ltx.DrawLatex(0.88,0.84,'#font[61]{pp#rightarrowHH#rightarrow#gamma#gammab#bar{b}}')
 
 leg = TLegend(0.15, 0.59, 0.40, 0.88, "brNDC")
 leg.SetTextSize(0.032)
@@ -165,7 +225,7 @@ latex.SetTextSize(24)
 latex.SetTextAlign(32)
 latex.SetTextFont(43)
 #latex.DrawLatex(0.89,.89,"#kappa_{t} = 1, c_{g} = c_{2g} = c_{2} = 0")
-latex.DrawLatex(0.88,.84,"c_{g} = c_{2g} = c_{2} = 0")
+latex.DrawLatex(0.88,.74,"c_{g} = c_{2g} = c_{2} = 0")
 latex.SetTextAlign(33)
 latex.SetTextSize(25)
 latex.SetTextAngle(-58)
@@ -175,6 +235,9 @@ latex.SetTextAngle(-82)
 latex.SetTextColor(cNicePurple)
 latex.DrawLatex(0.50,.45,"#kappa_{t} = 2")
 
+latex.SetTextAngle(-85)
+latex.SetTextColor(cNiceGreen)
+latex.DrawLatex(0.55,.45,"#kappa_{t} = "+str("%.1f" % kmax))
 
 c.Update()
 c.cd()
@@ -190,11 +253,17 @@ print 'Expected excluded range with kt = 1'
 Compare(nonresXSEC, plots['0.500'])
 print 'Expected excluded range with kt = 2'
 Compare(nonresXSEC_2, plots['0.500'])
+print 'Expected excluded kt_max'
+Compare(nonresXSEC_3, plots['0.500'])
+
 if(opt.unblind):
   print 'Observed excluded range with kt = 1'
   Compare(nonresXSEC, plots['-1'])
   print 'Observed excluded range with kt = 2'
   Compare(nonresXSEC_2, plots['-1'])
+  print 'Expected excluded kt_max'
+  Compare(nonresXSEC_3, plots['-1'])
+
 
 
 outfile.cd()
