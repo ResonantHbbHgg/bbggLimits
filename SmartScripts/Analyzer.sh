26368a912498b60fc38bf463cf7f7eeb491fd4a6
maxime <mgouzevi@cern.ch>
2018-10-16 11:27:59 +0200

    Updated

--- SmartScripts/Analyzer.sh
+++ SmartScripts/Analyzer.sh
@@ -15,9 +15,9 @@ combine -M ProfileLikelihood -d hhbbgg_13TeV_DataCard.root --significance -t -1 
 
 combine -M ProfileLikelihood -d hhbbgg_13TeV_DataCard.root --significance -t -1 --expectSignal 1 -m 125 -n SM_13TeV_3ab
 
-combine -M Asymptotic -d hhbbgg_13TeV_DataCard.root -t -1 --expectSignal 1 -m 125 -n SM_13TeV_3ab -S 0 &>Limit_stat.txt
+combine -M Asymptotic -d hhbbgg_13TeV_DataCard.root -t -1  --run blind --expectSignal 1 -m 125 -n SM_13TeV_3ab -S 0 &>Limit_stat.txt
 
-combine -M Asymptotic -d hhbbgg_13TeV_DataCard.root -t -1 --expectSignal 1 -m 125 -n SM_13TeV_3ab &>Limit.txt
+combine -M Asymptotic -d hhbbgg_13TeV_DataCard.root -t -1  --run blind --expectSignal 1 -m 125 -n SM_13TeV_3ab &>Limit.txt
 
 
 cd -
